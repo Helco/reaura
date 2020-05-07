0b6e295d8ca1a167626050232b51c9d35b714240
Helco <hermann.noll@hotmail.com>
2020-05-07 20:01:33 +0200

    Fix VariableSet not being initialized early enough

--- reaura/Assets/Runtime/AuraSOVariableSet.cs
+++ reaura/Assets/Runtime/AuraSOVariableSet.cs
@@ -24,14 +24,11 @@ namespace Aura
 
         [SerializeField]
         private List<Variable> values = new List<Variable>();
-        private Dictionary<string, int> valueIndices;
-
-        private void Awake()
-        {
-            valueIndices = values
+        private Dictionary<string, int> _valueIndices;
+        private Dictionary<string, int> ValueIndices => _valueIndices ??
+            (_valueIndices = values
                 .Select((p, i) => (name: p.Name, index: i))
-                .ToDictionary(p => p.name, p => p.index);
-        }
+                .ToDictionary(p => p.name, p => p.index));
 
         public IEnumerable<Variable> AsEnumerable => values;
 
@@ -39,17 +36,17 @@ namespace Aura
         {
             get
             {
-                if (!valueIndices.TryGetValue(name, out int index))
+                if (!ValueIndices.TryGetValue(name, out int index))
                     throw new ArgumentOutOfRangeException($"Unknown variable {name} in set {this.name}");
                 return values[index].Value;
             }
             set
             {
-                if (valueIndices.TryGetValue(name, out int index))
+                if (ValueIndices.TryGetValue(name, out int index))
                     values[index] = new Variable(name, value);
                 else
                 {
-                    valueIndices[name] = values.Count;
+                    ValueIndices[name] = values.Count;
                     values.Add(new Variable(name, value));
                 }
             }
