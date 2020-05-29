d8f0b4aef3186bca54fcccbadaf900676e264916
Helco <hermann.noll@hotmail.com>
2020-05-29 19:09:57 +0200

    Add various dummy script functions

--- Aura/Aura/src/Systems/DummyScriptSystem.cs
+++ Aura/Aura/src/Systems/DummyScriptSystem.cs
@@ -9,6 +9,14 @@ namespace Aura.Systems
         public void RegisterGameFunctions(Interpreter interpreter)
         {
             interpreter.RegisterFunction<string, int, int>("SetSoundVolume_And_Pan", (a, b, c) => DummyScriptFunction("SetSoundVolume_And_Pan"));
+            interpreter.RegisterFunction<string>("StopSound", a => DummyScriptFunction("StopSound"));
+            interpreter.RegisterFunction<string, int, int, string>("StartSound", (a,b,c,d) => DummyScriptFunction("StartSound"));
+            interpreter.RegisterFunction<string, int>("StartBackgroundSound", (a, b) => DummyScriptFunction("StartBackgroundSound"));
+            interpreter.RegisterFunction<string, int>("StartNoise", (a, b) => DummyScriptFunction("StartNoise"));
+            interpreter.RegisterFunction<string, string>("StopMP3", (a, b) => DummyScriptFunction("StopMP3"));
+            interpreter.RegisterFunction<string, int, string>("StartMP3", (a, b, c) => DummyScriptFunction("StartMP3"));
+            interpreter.RegisterFunction<int>("Fade", (a) => DummyScriptFunction("Fade"));
+            interpreter.RegisterFunction<int>("UnFade", (a) => DummyScriptFunction("UnFade"));
         }
 
         private void DummyScriptFunction(string name)
